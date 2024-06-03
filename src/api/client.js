e9c74d172fc3d47c8c48c9ea40932235453c5f5d
Himanshu <hgusain088@gmail.com>
2024-06-03 20:40:16 +0530

    error

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,6 @@
 import axios from "axios"
-
 const client = axios.create({
-  baseURL: "https://mern-cine-philes.vercel.app/api"
+  baseURL: "https://mern-cine-philes-ap-is.vercel.app/api"
 })
 
 export default client
