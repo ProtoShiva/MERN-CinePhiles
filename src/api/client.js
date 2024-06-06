df30b586dacc300094d740e5d061ebddb55625fd
Himanshu <hgusain088@gmail.com>
2024-06-06 15:37:07 +0530

    changed the server endpoint

--- src/api/client.js
+++ src/api/client.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 const client = axios.create({
-  baseURL: "http://localhost:8000/api"
+  baseURL: "https://mern-cine-philes-ap-is.vercel.app/api"
 })
 
 export default client
